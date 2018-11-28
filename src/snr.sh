c392412bc35442237a510ec108814d643b322273
Stefan Westerfeld <stefan@space.twc.de>
2018-11-28 12:39:23 +0100

    Add signal-to-noise ratio script.
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

---
+++ src/snr.sh
@@ -0,0 +1,9 @@
+# pseudo random pattern
+
+PATTERN=4e1243bd22c66e76c2ba9eddc1f91394e57f9f83
+
+for i in test/T*
+do
+  audiowmark add $i t.wav $PATTERN >/dev/null
+  echo $i $(audiowmark snr $i t.wav)
+done | grep snr | awk '{s += $2; n++} END { print s/n; }'
