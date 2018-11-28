7eca3f4120cf54f51ac72a508e9b9cfe917e8756
Stefan Westerfeld <stefan@space.twc.de>
2018-11-28 15:01:15 +0100

    ber-test.sh: fix quotes for filenames with spaces.
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- src/ber-test.sh
+++ src/ber-test.sh
@@ -6,7 +6,7 @@ TRANSFORM=$1
 for i in test/T*
 do
   echo $i
-  audiowmark add $i t.wav $PATTERN >/dev/null
+  audiowmark add "$i" t.wav $PATTERN >/dev/null
   if [ "x$TRANSFORM" == "xmp3" ]; then
     if [ "x$2" == "x" ]; then
       echo "need mp3 bitrate" >&2
@@ -27,5 +27,5 @@ do
     echo "unknown transform $TRANSFORM" >&2
     exit 1
   fi
-  audiowmark cmp $i t.wav $PATTERN
+  audiowmark cmp "$i" t.wav $PATTERN
 done | grep bit_error_rate | awk '{ er += $2; n++; if ($2 > max_er) max_er = $2;} END { print er / n, max_er; }'
