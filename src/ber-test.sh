853a6c831cbe4bf8f668a92896dcf04dbaa21b91
Stefan Westerfeld <stefan@space.twc.de>
2018-12-12 12:30:51 +0100

    Support AWM_SEEDS for testing with multiple --seed values.
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- src/ber-test.sh
+++ src/ber-test.sh
@@ -5,6 +5,9 @@ TRANSFORM=$1
 if [ "x$AWM_SET" == "x" ]; then
   AWM_SET=small
 fi
+if [ "x$AWM_SEEDS" == "x" ]; then
+  AWM_SEEDS=0
+fi
 
 {
   if [ "x$AWM_SET" == "xsmall" ]; then
@@ -17,54 +20,57 @@ fi
   fi
 } | while read i
 do
-  echo $i
-  audiowmark add "$i" t.wav $PATTERN $AWM_PARAMS >/dev/null
-  if [ "x$TRANSFORM" == "xmp3" ]; then
-    if [ "x$2" == "x" ]; then
-      echo "need mp3 bitrate" >&2
+  for SEED in $AWM_SEEDS
+  do
+    echo $i
+    audiowmark add "$i" t.wav $PATTERN $AWM_PARAMS --seed $SEED >/dev/null
+    if [ "x$TRANSFORM" == "xmp3" ]; then
+      if [ "x$2" == "x" ]; then
+        echo "need mp3 bitrate" >&2
+        exit 1
+      fi
+      lame -b $2 t.wav t.mp3 --quiet
+      rm t.wav
+      ffmpeg -i t.mp3 t.wav -v quiet -nostdin
+
+      # some (low) mpeg quality settings use a lower sample rate
+      if [ "x$(soxi -r t.wav)" != "x44100" ]; then
+        sox t.wav tr.wav rate 44100
+        mv tr.wav t.wav
+      fi
+    elif [ "x$TRANSFORM" == "xdouble-mp3" ]; then
+      if [ "x$2" == "x" ]; then
+        echo "need mp3 bitrate" >&2
+        exit 1
+      fi
+      # first mp3 step (fixed bitrate)
+      lame -b 128 t.wav t.mp3 --quiet
+      rm t.wav
+      ffmpeg -i t.mp3 t.wav -v quiet -nostdin
+
+      # second mp3 step
+      lame -b $2 t.wav t.mp3 --quiet
+      rm t.wav
+      ffmpeg -i t.mp3 t.wav -v quiet -nostdin
+
+      # some (low) mpeg quality settings use a lower sample rate
+      if [ "x$(soxi -r t.wav)" != "x44100" ]; then
+        sox t.wav tr.wav rate 44100
+        mv tr.wav t.wav
+      fi
+    elif [ "x$TRANSFORM" == "xogg" ]; then
+      if [ "x$2" == "x" ]; then
+        echo "need ogg bitrate" >&2
+        exit 1
+      fi
+      oggenc -b $2 t.wav -o t.ogg --quiet
+      oggdec t.ogg -o t.wav --quiet
+    elif [ "x$TRANSFORM" == "x" ]; then
+      :
+    else
+      echo "unknown transform $TRANSFORM" >&2
       exit 1
     fi
-    lame -b $2 t.wav t.mp3 --quiet
-    rm t.wav
-    ffmpeg -i t.mp3 t.wav -v quiet -nostdin
-
-    # some (low) mpeg quality settings use a lower sample rate
-    if [ "x$(soxi -r t.wav)" != "x44100" ]; then
-      sox t.wav tr.wav rate 44100
-      mv tr.wav t.wav
-    fi
-  elif [ "x$TRANSFORM" == "xdouble-mp3" ]; then
-    if [ "x$2" == "x" ]; then
-      echo "need mp3 bitrate" >&2
-      exit 1
-    fi
-    # first mp3 step (fixed bitrate)
-    lame -b 128 t.wav t.mp3 --quiet
-    rm t.wav
-    ffmpeg -i t.mp3 t.wav -v quiet -nostdin
-
-    # second mp3 step
-    lame -b $2 t.wav t.mp3 --quiet
-    rm t.wav
-    ffmpeg -i t.mp3 t.wav -v quiet -nostdin
-
-    # some (low) mpeg quality settings use a lower sample rate
-    if [ "x$(soxi -r t.wav)" != "x44100" ]; then
-      sox t.wav tr.wav rate 44100
-      mv tr.wav t.wav
-    fi
-  elif [ "x$TRANSFORM" == "xogg" ]; then
-    if [ "x$2" == "x" ]; then
-      echo "need ogg bitrate" >&2
-      exit 1
-    fi
-    oggenc -b $2 t.wav -o t.ogg --quiet
-    oggdec t.ogg -o t.wav --quiet
-  elif [ "x$TRANSFORM" == "x" ]; then
-    :
-  else
-    echo "unknown transform $TRANSFORM" >&2
-    exit 1
-  fi
-  audiowmark cmp t.wav $PATTERN $AWM_PARAMS
+    audiowmark cmp t.wav $PATTERN $AWM_PARAMS --seed $SEED
+  done
 done | grep bit_error_rate | awk '{ er += $2; n++; if ($2 > max_er) max_er = $2;} END { print er / n, max_er; }'
