c16d6dc0f959a25e05a70b889c08fa76354d44f9
Stefan Westerfeld <stefan@space.twc.de>
2018-12-12 17:29:38 +0100

    Fix seed-test with new AWM_SEEDS variable.
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- src/seed-test.sh
+++ src/seed-test.sh
@@ -8,5 +8,5 @@ echo "left          : $P1"
 echo "right         : $P2"
 for seed in $(seq 0 $MAX_SEED)
 do
-  echo $(AWM_PARAMS="$P1 --seed $seed" ber-test.sh "$@") $(AWM_PARAMS="$P2 --seed $seed" ber-test.sh "$@")
+  echo $(AWM_SEEDS=$seed AWM_PARAMS="$P1" ber-test.sh "$@") $(AWM_SEEDS=$seed AWM_PARAMS="$P2" ber-test.sh "$@")
 done | awk '{a += $1; b += $2; c += $3; d += $4; n++; } {printf ("%.5f %.5f     -     %.5f %.5f    -    (((%s)))\n", a/n, b/n, c/n, d/n, $0);}'
