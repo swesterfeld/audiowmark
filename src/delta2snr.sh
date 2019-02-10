399de99d0de56e186527343e9744c533dabef78a
Stefan Westerfeld <stefan@space.twc.de>
2019-02-10 13:54:06 +0100

    Adapt snr related scripts to new command line options (--strength/--snr).
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- src/delta2snr.sh
+++
@@ -1,6 +0,0 @@
-for delta in 0.030 0.020 0.015 0.010 0.005 0.003 0.002 0.001
-do
-  #echo $delta $(AWM_PARAMS="--water-delta=$delta" ber-test.sh double-mp3 128) $(AWM_PARAMS="--water-delta=$delta" ber-test1.sh double-mp3 128)
-  #echo $delta $(AWM_PARAMS="--water-delta=$delta" peaq.sh)
-  echo $delta $(AWM_PARAMS="--water-delta=$delta" snr.sh)
-done
