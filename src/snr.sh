ee975505228bb46737da2973affe856297b1d27a
Stefan Westerfeld <stefan@space.twc.de>
2020-03-15 15:11:19 +0100

    Fix snr script (audiowmark with streaming uses stderr for messages).
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- src/snr.sh
+++ src/snr.sh
@@ -4,5 +4,5 @@ PATTERN=4e1243bd22c66e76c2ba9eddc1f91394
 
 for i in test/T*
 do
-  echo $i $(audiowmark add $i t.wav $PATTERN $AWM_PARAMS --snr | grep SNR)
+  echo $i $(audiowmark add $i t.wav $PATTERN $AWM_PARAMS --snr 2>&1 | grep SNR)
 done | awk '{s += $3; n++} END { print s/n; }'
