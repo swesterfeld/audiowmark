e1e4aa738a2e8c02c4d1c6148e0dbeae1bfdd2d2
Stefan Westerfeld <stefan@space.twc.de>
2020-03-17 15:33:52 +0100

    Improve short clip error test adoc.
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- src/gen-short-clip-adoc.sh
+++ src/gen-short-clip-adoc.sh
@@ -5,6 +5,28 @@ STR_CLIPS="5 10 15 20 25 30"
 MAIN_CLIPS="5 10 15 20 30 40 50 60"
 
 echo ".sync-codec-resistence$TRUNC"
+echo '[frame="topbot",options="header",cols="<2,8*>1"]'
+echo '|=========================='
+echo -n "| "
+for CLIP in $MAIN_CLIPS
+do
+  echo -n "| $CLIP"
+done
+echo
+
+for TEST in mp3-256 mp3-128 double-mp3-128 ogg-128
+do
+  echo -n "| $TEST "
+  for CLIP in $MAIN_CLIPS
+  do
+    cat main-$TEST-*-$CLIP | awk '{bad += $1; n += $2} END {if (n==0) n=1;fer=100.0*bad/n; bold=fer>0?"*":" ";printf ("| %s%.2f%s", bold, fer, bold)}'
+  done
+  echo
+done
+echo
+echo '|=========================='
+
+echo ".sync-codec-resistence$TRUNC"
 echo '[frame="topbot",options="header",cols="<2,6*>1"]'
 echo '|=========================='
 echo -n "| "
