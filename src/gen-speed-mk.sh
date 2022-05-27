d6640dd98b51e46332cae724cb8cb49db5f18186
Stefan Westerfeld <stefan@space.twc.de>
2022-05-27 17:15:59 +0200

    Test patient speed detection in speed test Makefile generator.
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- src/gen-speed-mk.sh
+++ src/gen-speed-mk.sh
@@ -3,20 +3,24 @@
 SEEDS=$(seq 10)
 STRENGTHS="10 15"
 CLIPS="15 30"
+PMODES="0 1"
 
 echo -n "all:"
 
 for SEED in $SEEDS
 do
-  for STRENGTH in $STRENGTHS
+  for PMODE in $PMODES
   do
-    # clips
-    for CLIP in $CLIPS
+    for STRENGTH in $STRENGTHS
     do
-      echo -n " speed-$CLIP-$STRENGTH-$SEED"
+      # clips
+      for CLIP in $CLIPS
+      do
+        echo -n " speed-$CLIP-$STRENGTH-$PMODE-$SEED"
+      done
+      # full file
+      echo -n " speed-full-$STRENGTH-$PMODE-$SEED"
     done
-    # full file
-    echo -n " speed-full-$STRENGTH-$SEED"
   done
 done
 
@@ -25,22 +29,30 @@ echo
 
 for SEED in $SEEDS
 do
-  for STRENGTH in $STRENGTHS
+  for PMODE in $PMODES
   do
-    # clips
-    for CLIP in $CLIPS
+    if [ "x$PMODE" == "x0" ]; then
+      PATIENT=""
+    else
+      PATIENT="AWM_SPEED_PATIENT=1"
+    fi
+    for STRENGTH in $STRENGTHS
     do
-      FILE="speed-$CLIP-$STRENGTH-$SEED"
+      # clips
+      for CLIP in $CLIPS
+      do
+        FILE="speed-$CLIP-$STRENGTH-$PMODE-$SEED"
+        echo "$FILE:"
+        echo -e "\t( cd ..; AWM_RAND_PATTERN=1 AWM_SET=huge2 AWM_PARAMS='--strength $STRENGTH' AWM_SPEED=1 $PATIENT AWM_SPEED_PRE_MP3=128 AWM_CLIP='$CLIP' AWM_SEEDS=$SEED AWM_FILE='t-$FILE' ber-test.sh mp3 128 ) >x$FILE"
+        echo -e "\tmv x$FILE $FILE"
+        echo
+      done
+      # full file
+      FILE="speed-full-$STRENGTH-$PMODE-$SEED"
       echo "$FILE:"
-      echo -e "\t( cd ..; AWM_RAND_PATTERN=1 AWM_SET=huge2 AWM_PARAMS='--strength $STRENGTH' AWM_SPEED=1 AWM_SPEED_PRE_MP3=128 AWM_CLIP='$CLIP' AWM_SEEDS=$SEED AWM_FILE='t-$FILE' ber-test.sh mp3 128 ) >x$FILE"
+      echo -e "\t( cd ..; AWM_RAND_PATTERN=1 AWM_SET=huge2 AWM_PARAMS='--strength $STRENGTH' AWM_SPEED=1 $PATIENT AWM_SPEED_PRE_MP3=128 AWM_SEEDS=$SEED AWM_FILE='t-$FILE' ber-test.sh mp3 128 ) >x$FILE"
       echo -e "\tmv x$FILE $FILE"
       echo
     done
-    # full file
-    FILE="speed-full-$STRENGTH-$SEED"
-    echo "$FILE:"
-    echo -e "\t( cd ..; AWM_RAND_PATTERN=1 AWM_SET=huge2 AWM_PARAMS='--strength $STRENGTH' AWM_SPEED=1 AWM_SPEED_PRE_MP3=128 AWM_SEEDS=$SEED AWM_FILE='t-$FILE' ber-test.sh mp3 128 ) >x$FILE"
-    echo -e "\tmv x$FILE $FILE"
-    echo
   done
 done
