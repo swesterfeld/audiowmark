c6a068db302fa9f5c51f0b3dd5c2b0f37d94330c
Stefan Westerfeld <stefan@space.twc.de>
2022-05-31 16:35:30 +0200

    TESTS: add synchronization tests
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- tests/clip-decoder-test.sh
+++ tests/clip-decoder-test.sh
@@ -4,10 +4,13 @@ source test-common.sh
 
 IN_WAV=clip-decoder-test.wav
 OUT_WAV=clip-decoder-test-out.wav
+CUT_WAV=clip-decoder-test-out-cut.wav
 
 $AUDIOWMARK test-gen-noise $IN_WAV 30 44100 || die "failed to generate noise"
 audiowmark_add $IN_WAV $OUT_WAV $TEST_MSG
 audiowmark_cmp $OUT_WAV $TEST_MSG
+$AUDIOWMARK cut-start $OUT_WAV $CUT_WAV 44300 || die "failed to cut input"
+audiowmark_cmp $CUT_WAV $TEST_MSG
 
-rm $IN_WAV $OUT_WAV
+rm $IN_WAV $OUT_WAV $CUT_WAV
 exit 0
