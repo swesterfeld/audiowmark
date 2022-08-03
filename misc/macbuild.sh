78f24e7efa97f8aef344059234a25ed8d3f9ed5a
Stefan Westerfeld <stefan@space.twc.de>
2022-08-03 21:34:59 +0200

    MISC: add more brew dependencies for mac builds
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- misc/macbuild.sh
+++ misc/macbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -Eeuo pipefail -x
 
-brew install autoconf-archive automake libsndfile fftw
+brew install autoconf-archive automake libsndfile fftw mpg123 libgcrypt
 ./autogen.sh
 make
 make check
