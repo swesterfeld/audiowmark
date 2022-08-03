cb7b119ba0ac635f74225d9bc61b7f0a4a2f2270
Stefan Westerfeld <stefan@space.twc.de>
2022-08-03 21:47:34 +0200

    MISC: build zita-resampler in macbuild.sh
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- misc/macbuild.sh
+++ misc/macbuild.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -Eeuo pipefail -x
 
+# install dependencies
 brew install autoconf-archive automake libsndfile fftw mpg123 libgcrypt
+
+# build zita-resampler
+git clone https://github.com/digital-stage/zita-resampler
+cd zita-resampler
+cmake .
+make install
+cd ..
+
+# build audiowmark
 ./autogen.sh
 make
 make check
