b0e2f81a32c50d34d15fde6b3ce8993c15c2a85e
Stefan Westerfeld <stefan@space.twc.de>
2024-06-03 14:57:55 +0200

    Bump copyright date in wav-pipe format.
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- src/wavpipeinputstream.cc
+++ src/wavpipeinputstream.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018-2020 Stefan Westerfeld
+ * Copyright (C) 2018-2024 Stefan Westerfeld
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
