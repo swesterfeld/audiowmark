113815ae8cd6eeaa8753c2e215610fd15666a470
Stefan Westerfeld <stefan@space.twc.de>
2024-06-06 16:01:01 +0200

    TESTS: add a few more checks to existing tests
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- tests/pipe-test.sh
+++ tests/pipe-test.sh
@@ -10,5 +10,7 @@ cat $IN_WAV | audiowmark_add - - $TEST_MSG > $OUT_WAV || die "watermark from pip
 audiowmark_cmp --expect-matches 5 $OUT_WAV $TEST_MSG
 cat $OUT_WAV | audiowmark_cmp --expect-matches 5 - $TEST_MSG || die "watermark detection from pipe failed"
 
+check_length $IN_WAV $OUT_WAV
+
 rm $IN_WAV $OUT_WAV
 exit 0
