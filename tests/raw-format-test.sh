6ee4088094dc3dcbeb5f0704a94ce63ce19655c4
Stefan Westerfeld <stefan@space.twc.de>
2024-12-24 17:15:37 +0100

    raw-format-test.sh: also validate 8-bit raw input/output
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- tests/raw-format-test.sh
+++ tests/raw-format-test.sh
@@ -19,7 +19,7 @@ raw_test()
     ffmpeg -v quiet -f $FFMPEG_FMT -ar 44100 -ac 2 -i - $OUT_WAV
 
   audiowmark_cmp --expect-matches 5 $OUT_WAV $TEST_MSG
-  check_snr $IN_WAV $OUT_WAV 32.4
+  check_snr $IN_WAV $OUT_WAV $SNR
 
   ffmpeg -v quiet -i $IN_WAV -f $FFMPEG_FMT -c:a pcm_$FFMPEG_FMT - | \
     audiowmark_add - $OUT2_WAV $TEST_MSG --input-format raw --raw-rate 44100 $AWM_FMT --test-no-limiter
@@ -30,7 +30,17 @@ raw_test()
   rm -f $IN_WAV $OUT_WAV $OUT2_WAV
 }
 
+
+## 8 bit
+
+SNR=31
+
+raw_test s8 --raw-bits 8
+raw_test u8 --raw-bits 8 --raw-encoding unsigned
+
 ## little endian
+
+SNR=32.4
 
 raw_test s16le
 raw_test s24le --raw-bits 24
