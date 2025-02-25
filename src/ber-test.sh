f9c1dd47869950661bd6a136f155780379dd3763
Stefan Westerfeld <stefan@space.twc.de>
2025-02-25 12:36:34 +0100

    Tell ffmpeg that our input file format is mp3.
    This avoids occasional automatic format detection fails.
    
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- src/ber-test.sh
+++ src/ber-test.sh
@@ -92,7 +92,7 @@ do
         # first (optional) mp3 step: simulate quality loss before speed change
         lame -b "$AWM_SPEED_PRE_MP3" ${AWM_FILE}.wav ${AWM_FILE}.mp3 --quiet
         rm ${AWM_FILE}.wav
-        ffmpeg -i ${AWM_FILE}.mp3 ${AWM_FILE}.wav -v quiet -nostdin
+        ffmpeg -f mp3 -i ${AWM_FILE}.mp3 ${AWM_FILE}.wav -v quiet -nostdin
       fi
 
       [ -z $SPEED_SEED ] && SPEED_SEED=0
@@ -128,7 +128,7 @@ do
       # first mp3 step (fixed bitrate)
       lame -b 128 ${AWM_FILE}.wav ${AWM_FILE}.mp3 --quiet
       rm ${AWM_FILE}.wav
-      ffmpeg -i ${AWM_FILE}.mp3 ${AWM_FILE}.wav -v quiet -nostdin
+      ffmpeg -f mp3 -i ${AWM_FILE}.mp3 ${AWM_FILE}.wav -v quiet -nostdin
 
       # second mp3 step
       lame -b $2 ${AWM_FILE}.wav ${AWM_FILE}.mp3 --quiet
